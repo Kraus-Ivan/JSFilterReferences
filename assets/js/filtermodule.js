9781d26a59235fdda8b5c81f31531f27f535a020
Ivan Kraus <ivan.kraus.021@pslib.cz>
2023-05-01 22:39:25 +0200

    Speedrunning

--- assets/js/filtermodule.js
+++ assets/js/filtermodule.js
@@ -5,51 +5,47 @@ export default function (menu, itemList) {
 
   let filterList = [];
   for (const item of itemList) {
-    if (item.dataset.filter !== undefined) {
-      let keywords = item.dataset.filter.split(";");
-      let texts = item.dataset.filtertext?.split(";");
+    if (item.dataset.filtertext !== undefined) {
+      let texts = item.dataset.filtertext.toLowerCase().split(";");
 
-      if (texts) {
-        for (let i = 0; i < keywords.length; i++) {
-          filterList.push({ keyword: keywords[i], text: texts[i] });
-        }
+      for (const text of texts) {
+        filterList.push(text);
       }
     }
   }
 
-  let uniqueFilterList = filterList.reduce((unique, item) => {
-    if (!unique.find((x) => x.keyword === item.keyword)) {
-      unique.push(item);
-    }
-    return unique;
-  }, []);
+  let uniqueFilterList = [...new Set(filterList)]; // omg takhle se dělá unikátní pole? :O // tento komentář za mě doslova napsal copilot achjo mám pocit, že budeme nahrazeni AI
 
   for (const filterItem of uniqueFilterList) {
     const button = document.createElement("a");
-    button.href = `?search=${filterItem.keyword}`;
+    const normalizedFilterItem = normalizeText(filterItem); // search-string friendly
+    button.href = `?search=${normalizedFilterItem}`; // todle nevím, jestli vůbec je legální nebo to máme dělat jinak
     button.classList.add("btn", "btn__filter");
-    button.dataset.filter = filterItem.keyword;
+    button.dataset.filter = normalizedFilterItem;
 
-    const textNode = document.createTextNode(filterItem.text);
-    button.appendChild(textNode);
+    const textNode = document.createTextNode(filterItem);
+    button.appendChild(textNode); 
 
     button.addEventListener("click", (e) => {
       e.preventDefault();
 
-      // Zrušení aktivního stavu tlačítka
+      // Zruší aktuálně aktivní tlačítko
       for (const btn of menu.querySelectorAll(".btn__filter")) {
         btn.classList.remove("btn__filter--active");
       }
-      // Aktivní aktuální tlačítko
+      // Nastaví novému tlačítku aktivní třídu
       button.classList.add("btn__filter--active");
 
-      // Filtrování
+      // Aplikuje filtr na obrázky
       applyFilter(button.dataset.filter);
-      // URL
+
+      // Aktualizuje URL
       updateURL(button.dataset.filter);
     });
 
     menu.appendChild(button);
+
+    // Aplikuje výchozí filtr při načtení stránky
     applyFilter("all");
   }
 
@@ -67,18 +63,27 @@ export default function (menu, itemList) {
   }
 }
 
+function normalizeText(text) {
+  return removeDiacritics(text.toLowerCase()).replace(/\s+/g, "-");
+}
+
 function applyFilter(filterKeyword) {
   for (const item of referenceItems) {
+    // ověří, jestli má item atribut data-filtertext
     if (item.dataset.filtertext !== undefined) {
       if (
+        // Pokud je filterKeyword "all" nebo se nachází filterKeyword ve filtertextu, tak zobrazí item
         filterKeyword === "all" ||
-        item.dataset.filter?.includes(filterKeyword)
-      ) {
-        item.style.display = "block";
-      } else {
+        normalizeText(item.dataset.filtertext).includes(filterKeyword)
+      ) 
+      { item.style.display = "block"; } 
+      // pokud se filterKeyword nenachází ve filtertextu, tak item skryje
+      else {
         item.style.display = "none";
       }
-    } else {
+    } 
+    // pokud item nemá atribut data-filtertext, tak ho skryje
+    else {
       item.style.display = "none";
     }
   }
@@ -92,4 +97,43 @@ function updateURL(filterValue) {
     "",
     window.location.pathname + "?" + parsedParams.toString()
   );
+}
+
+function removeDiacritics(str) {
+  const diacriticsMap = [
+    { base: "a", chars: "á" },
+    { base: "c", chars: "č" },
+    { base: "d", chars: "ď" },
+    { base: "e", chars: "éě" },
+    { base: "i", chars: "í" },
+    { base: "n", chars: "ň" },
+    { base: "o", chars: "ó" },
+    { base: "r", chars: "ř" },
+    { base: "s", chars: "š" },
+    { base: "t", chars: "ť" },
+    { base: "u", chars: "úů" },
+    { base: "y", chars: "ý" },
+    { base: "z", chars: "ž" },
+  ];
+
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    let foundMatch = false;
+
+    for (const entry of diacriticsMap) {
+      if (entry.chars.includes(currentChar)) {
+        result += entry.base;
+        foundMatch = true;
+        break;
+      }
+    }
+
+    if (!foundMatch) {
+      result += currentChar;
+    }
+  }
+
+  return result;
 }
